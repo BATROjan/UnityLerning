faa1e608a4e771103cad774dcba9b638d0783792
BATROjan <batir0405200@gmail.com>
2024-09-02 14:46:42 +0700

    save

---
+++ Assets/DogMoveComponent.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogMoveComponent : MonoBehaviour
+{
+  public float MoveSpeed = 1f;
+  
+}
---
+++ Assets/UserInputSystem.cs
@@ -0,0 +1,24 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class UserInputSystem : ComponentSystem
+    {
+        private EntityQuery _inputQuery;
+        
+        protected override void OnCreate()
+        {
+            if (Input.touches.Length == 1)
+            {
+                
+            }
+          // _inputQuery = GetEntityQuery(ComponentType.ReadOnly<userin>());
+        }
+
+        protected override void OnUpdate()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
